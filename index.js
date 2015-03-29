478a5bd8a3f71031ea7cc3d0dcf8d086643d7d9b
npenin <nicolas.penin@dragon-angel.fr>
2015-03-29 19:20:38 +0200

    Handled completed callback

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports={name:"http", "trigger":{}, "actions":[{name:"get", fields:[{ name:"url", displayName:"URL"}], delegate:function(fields){
-        var result= function(fields){
+        var result= function(fields, trigger, completed){
 			try
 			{
 				var url=$('url').parse(fields.url);
-				
-				$('http').request({hostname:url.hostname, path:url.path, port:url.port, headers:{accept:'application/json'}}).on('clienterror', function(ex){ console.log(ex); }).on('error', function(ex){ console.log(ex); }).on('response', function(response){ }).end();
+				console.log(fields.url);
+				console.log(url);
+				$('http').request({hostname:url.hostname, path:url.path, port:url.port, headers:{accept:'application/json'}}).on('clienterror', function(ex){ console.log(ex); }).on('error', function(ex){ console.log(ex); }).on('response', function(response){ completed();  }).end();
 			}
 			catch (ex)
 			{
