6907ecc41b2c27be41baed76bcf51fbdb8f363b4
npenin <nicolas.penin@dragon-angel.fr>
2016-01-25 07:02:37 +0100

    handled https

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+
+function mkdirp(path, callback)
+{
+    $('fs').exists(path, function(exists){
+        if(!exists)
+            mkdirp($('path').dirname(path), function(err){
+                if(err)
+                    callback(err);
+                else
+                    $('fs').mkdir(path, callback)
+            });
+        else
+            callback();
+    })
+}
+
 module.exports={name:"http", "trigger":{}, "actions":[{name:"get", fields:[{ name:"url", displayName:"URL"}], delegate:function(fields){
         var result= function(fields, trigger, completed){
 			try
@@ -5,14 +21,44 @@ module.exports={name:"http", "trigger":{}, "actions":[{name:"get", fields:[{ nam
 				var url=$('url').parse(fields.url);
 				console.log(fields.url);
 				console.log(url);
-				$('http').request({hostname:url.hostname, path:url.path, port:url.port, headers:{accept:'application/json'}})
-				    .on('clienterror', function(ex){ console.log(ex); })
-				    .on('error', function(ex){ console.log(ex); })
-				    .on('response', function(response){ 
-				        if(completed)    
-				            completed();  
-		            })
-			        .end();
+				var requestor;
+				if(url.protocol=='https:' || url.protocol=='https')
+				    requestor=$('https');
+			    else
+			        requestor=$('http');
+				requestor.request({hostname:url.hostname, path:url.path, port:url.port, headers:{accept:'application/json'}})
+                    .on('clienterror', function(ex){ console.log(ex); })
+                    .on('error', function(ex){ console.log(ex); })
+                    .on('response', function(response){ 
+                        if(completed)    
+                            completed();  
+                    })
+                    .end();
+			}
+			catch (ex)
+			{
+				console.log(ex);
+			}
+        };
+        result.fields=fields;
+        return result;
+}},
+{name:"post", fields:[{ name:"url", displayName:"URL"},{ name:"body", displayName:"Corps"}], delegate:function(fields){
+        var result= function(fields, trigger, completed){
+			try
+			{
+				var url=$('url').parse(fields.url);
+				console.log(fields.url);
+				console.log(url);
+				var req=$('http').request({method:'post', hostname:url.hostname, path:url.path, port:url.port, headers:{accept:'application/json'}})
+                    .on('clienterror', function(ex){ console.log(ex); })
+                    .on('error', function(ex){ console.log(ex); })
+                    .on('response', function(response){ 
+                        if(completed)    
+                            completed();  
+                    });
+                req.write(fields.body);
+                req.end();
 			}
 			catch (ex)
 			{
@@ -30,14 +76,16 @@ module.exports={name:"http", "trigger":{}, "actions":[{name:"get", fields:[{ nam
                 if(fields.userName)
                     $.extend(options, {auth:fields.userName+':'+fields.password});
 				$.ajax(fields.url, options)
-                    .on('response', function(response){ 
-                        var output = $('fs').createWriteStream(fields.file);
-                            response.pipe(output);
-                        response.on('close', function(){
-                            console.log('download complete');
-                            if(completed)
-                                completed();
-                        });
+                    .on('response', function(response){
+                        mkdirp($('path').dirname(fields.file), function(){
+                            var output = $('fs').createWriteStream(fields.file);
+                                response.pipe(output);
+                            response.on('close', function(){
+                                console.log('download complete');
+                                if(completed)
+                                    completed();
+                            });
+                        })
                     })
                     .end();
 			}
